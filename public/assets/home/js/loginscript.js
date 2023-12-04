30c07ffd8139a6f4b47e147c74ee7a92917fbbc7
Bikash Subedi <biikashsubedi@gmail.com>
2023-12-04 15:15:49 +0545

    fix for login page

--- public/assets/home/js/loginscript.js
+++ public/assets/home/js/loginscript.js
@@ -1,7 +1,6 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
-container.classList.remove("active");
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
@@ -9,18 +8,3 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
-
-function isRegisterPage()
-{
-    return window.location.pathname.includes("/register")
-
-}
-
-if (isRegisterPage()){
-    container.classList.add("active")
-}
-
-function isContainerActive()
-{
-    return container.classList.contains("active")
-}
